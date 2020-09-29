2199ae4d1565e59d015a743c11784b0ac0641680
Jason Wherry <jw473415@ohio.edu>
2020-09-29 19:52:58 -0400

    Added two images, uploaded first podcast, Changed sub-memo in top banner, took AthensTune01.m4a down, temporarily removed download button.

--- js/showMore.js
+++ js/showMore.js
@@ -1,3 +1,24 @@
-$('.carousel').carousel({
-  interval: 2000
-})
+// // $('.carousel').carousel({
+// //   interval: 2000
+// // })
+
+
+// // open image in new tab
+// function clickImage(){
+
+// 	// variables
+// 	var outerDiv = document.getElementsByClassName("carousel-inner")[0];
+// 	var div = document.getElementById("img").getElementsByClassName("carousel-item active")[0];
+// 	var img = div.getElementsByClassName("d-block w-100")[0];
+// 	var url = img.src;
+
+// 	// Alert Messages
+// 	// alert(outerDiv)
+// 	// alert("Carousel-item: " + div);
+// 	// alert("IMG element: " + img);
+// 	// alert("URL: " + url)
+
+// 	// open image in new window
+// 	window.open(url, 'Image');
+
+// }
